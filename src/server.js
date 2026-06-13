6f35c5b42f6f71b8afdca303fb205a1943908305
Your Name <your-email@example.com>
2026-06-13 22:55:05 +0300

    1

--- src/server.js
+++ src/server.js
@@ -19,7 +19,11 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 // ======================
 app.use(cors({
-  origin: 'http://localhost:5173' || 'https://kino-book.vercel.app',
+  origin: [
+    'http://localhost:5173',
+    'https://kino-book.vercel.app',           // ← твой домен на Vercel
+    /\.vercel\.app$/                          // ← разрешает все поддомены vercel
+  ],
   credentials: true
 }));
 app.use(express.json());
